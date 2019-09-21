27c9308a11d4522ffacb10f610162ea2ebc6b05a
David Moll <39961794+Akashic101@users.noreply.github.com>
2019-09-21 13:10:30 +0200

    Add files via upload

--- src/game/LightsOutJava.java
+++ src/game/LightsOutJava.java
@@ -6,61 +6,110 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class LightsOutJava extends JFrame {
 
-	private static final long serialVersionUID = 1L; // Erstellt eine Seriennummer
+	private static final long serialVersionUID = 4035384304216493735L; // Erstellt eine Seriennummer
 	private Container fenster; // Erstellt einen Container
 	private JButton[][] buttons; // Erstellt ein 2-dimensoniales Array des typen JButton
+	private int amountMoves = 0;
+	JLabel moves = new JLabel("Moves: " + amountMoves);
 
 	public LightsOutJava(int x, int y) {
-		buttons = new JButton[x][y]; // Erstellt eine neue Liste der Größe die im Konstruktor angegeben wird, wobei x die Anzahl der Spalten und y die Anzahl der Reihen ist
-		fenster = getContentPane();	//Gibt das contentPane-Objekt für diesen Rahmen zurück
+		buttons = new JButton[x][y]; // Erstellt eine neue Liste der Größe die im Konstruktor angegeben wird, wobei x
+										// die Anzahl der Spalten und y die Anzahl der Reihen ist
+		fenster = getContentPane(); // Gibt das contentPane-Objekt für diesen Rahmen zurück
 		fenster.setLayout(new GridLayout(0, y)); // Erstellt ein neues Grid-Layout der Größe die im Konstruktor
-														// angegeben wird
+													// angegeben wird
+
+		int counter = 0;
 		for (int i = 0; i < buttons.length; i++) {
 			for (int j = 0; j < buttons[i].length; j++) {
-				buttons[i][j] = new JButton("" + (i + 1) * (j + 1)); // Erstellt einen neuen Button mit dem Text (i+1) +
-																		// (j+1)
+				counter++;
+				buttons[i][j] = new JButton("" + counter); // Erstellt einen neuen Button mit dem Text (i+1) +
+															// (j+1)
 				buttons[i][j].setBackground(Color.white); // Setzt den Hintergrund auf Weiß
 				buttons[i][j].addActionListener(new ButtonPress()); // Fügt die Klasse ButtonPress als Actionlistener
 																	// dem Button hinzu
+				buttons[i][j].addActionListener(new AmountMoves());
 				fenster.add(buttons[i][j]); // Fügt den Button dem Container hinzu
+				
 			}
 		}
-		setSize(5 * 100, 5 * 100); // Setzt die Größe des Containers auf 500 x 500
+
+		fenster.add(moves);
+
+		setSize(x * 100, y * 100); // Setzt die Größe des Containers auf 500 x 500
 
 		Random random = new Random(); // Erstellt einen neuen Random
 		for (JButton[] buttonrow : buttons) { // Geht einmal durch das JButton-Array
 			for (JButton button : buttonrow) {
-				boolean temp = random.nextBoolean(); // Setzt einen zufälligen Boolean bei jedem neuem Button
-				if (temp == true) { // Wenn temp true ist
+				boolean bool = random.nextBoolean(); // Setzt einen zufälligen Boolean bei jedem neuem Button
+				if (bool == true) { // Wenn bool true ist
 					button.doClick(); // wird der Button angeklickt und ButtonPress wird ausgeführt
-
+					amountMoves--;
 				}
 			}
 		}
+
+		for (int i = 0; i < buttons.length; i++) {
+			for (int j = 0; j < buttons[i].length; j++) {
+				buttons[i][j].setEnabled(false);
+			}
+		}
+
 		fenster.repaint(); // Updatet das Fenster
 		setVisible(true); // Setzt das Fenster auf sichtbar
+
+		JButton start = new JButton("Start");
+		fenster.add(start);
+		start.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < buttons.length; i++) {
+					for (int j = 0; j < buttons[i].length; j++) {
+						buttons[i][j].setEnabled(true);
+						start.setVisible(false);
+					}
+				}
+			}
+		});
+
+	}
+
+	private class AmountMoves implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			amountMoves++;
+			moves.setText("Moves: " + amountMoves);
+		}
+
 	}
 
 	private class ButtonPress implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
+
 			int x = 0;
 			int y = 0;
 			for (int i = 0; i < buttons.length; i++) { // Geht einmal durch das gesamte Array
 				for (int j = 0; j < buttons[i].length; j++) {
 					if (((JButton) e.getSource()).equals(buttons[i][j])) { // Wenn die Source des angeklickten Buttons
 						x = i; // Setzt X (und Y in Z.58) auf den Wert die Position des Buttons damit diese im
-						// switchColor benutzt werden können
+								// switchColor benutzt werden können
 						y = j;
 						break; // Beendet die Schleife
 					}
 				}
 			}
+
 			switchColor(buttons[x][y]); // Ändert die Farbe des Buttons links vom angeklickten Button
 			if (x > 0) {
 				switchColor(buttons[x - 1][y]);
@@ -75,6 +124,27 @@ public class LightsOutJava extends JFrame {
 				switchColor(buttons[x][y + 1]);
 			}
 
+			if (checkWin() == true) {// Checks for a winner and executes freeze and toggles gameDone variable
+				for (JButton[] buttonrow : buttons) {
+					for (JButton button : buttonrow) {
+						button.setBackground(Color.green);
+						button.setForeground(Color.black);
+						button.setEnabled(false);
+					}
+				}
+			}
+
+		}
+
+		private boolean checkWin() {
+			for (JButton[] buttonrow : buttons) {
+				for (JButton button : buttonrow) {
+					if (button.getBackground() == Color.black) {
+						return false;
+					}
+				}
+			}
+			return true;
 		}
 
 		public void switchColor(JButton button) { // Ändert die Farbe des angeklickten Buttons
@@ -86,5 +156,6 @@ public class LightsOutJava extends JFrame {
 				button.setForeground(Color.white);// Und die Farbe des Textes wird auf Weiß gesetzt
 			}
 		}
+
 	}
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -3,16 +3,19 @@ package game;
 import java.io.IOException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		LightsOutJava game = new LightsOutJava(5, 5); // Erstellt ein neues Exemplar von LightsOutJava mit der Gridgröße 5 und 5
+		String input= JOptionPane.showInputDialog("Wie groß soll das Spielfeld sein?"); //Fragefeld was die Größe des Spielfeldes abfragt
+
+		LightsOutJava game = new LightsOutJava(Integer.parseInt(input), Integer.parseInt(input)); // Erstellt ein neues Exemplar von LightsOutJava mit der Gridgröße input(x) und input(y)
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Wenn der X-Button gedrückt wird wird die Anwendung geschlossen
 		game.setTitle("LightsOut"); // Setzt den Namen der Anwendung
 		ImageIcon JFrameIcon = new ImageIcon("Icon/LightsOut.png");	//Erstellt einen neuen ImageIcon
-		game.setIconImage(JFrameIcon.getImage());	//Setzt den Icon der Anwendung auf den Pfad von JFrameIcon
+		game.setIconImage(JFrameIcon.getImage()); //Setzt den Icon der Anwendung auf den Pfad von JFrameIcon
 	}
 
 }
